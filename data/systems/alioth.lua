2160fdfafb13fe35bc2f5a39b0821a76b763d265
Robert Norris <rob@eatenbyagrue.org>
2011-02-08 09:11:22 +1100

    naming is serious business

--- data/systems/alioth.lua
+++ data/systems/alioth.lua
@@ -4,9 +4,9 @@ define_system('Alioth', {
     sector = { 0, 4 },
     pos = v(0.629,0.880,1.045),
     seed = 5,
-    gov = GOV_CISLIBDEM,
-    short_description = 'Liberal Democracy',
-    long_description = [[Alioth comes from the ancient Arabic word alyat - fat tail of sheep
+    govtype = GOV_CISLIBDEM,
+    short_desc = 'Liberal Democracy',
+    long_desc = [[Alioth comes from the ancient Arabic word alyat - fat tail of sheep
 The two massive ark ships bound for Alioth were the first of many such sub-light vessels designed specifically for the kind of lengthy journeys required to spread to the stars.
 The twin ships departed on the year 2354 to commemorate the journey to Epsilon Eridani. The journey was uneventful and was completed in a little over 127 years, owing to the fact that propulsion technology was far geater than it had been in the past.
 The colonists had a hard life, high gravity slowed down any progress that should have been made. Communication with Earth was non-existent, politics and the 80 year time-lag made it impossible. By the late 26th Century Alioth had progressed a long way and any ties to Earth had long been lost. However with the advent of Hyper-Space Technology in the late 26th Century, Earth began to show an interest in Alioth once more and Alioth was no longer a self-governed world.
