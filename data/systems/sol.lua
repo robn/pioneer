18c2908511ca14e19d25b7f0bdcd7e3f0ba2d525
Robert Norris <rob@eatenbyagrue.org>
2011-02-08 19:59:20 +1100

    sol, just the star

---
+++ data/systems/sol.lua
@@ -0,0 +1,7 @@
+define_system('Sol', {
+	type = { TYPE_STAR_G },
+	sector = { 0, 0 },
+	pos = v(0.5,0.5,0),
+	short_desc = 'The historical birthplace of humankind',
+	long_desc = [[Sol is a fine joint]],
+})
