67a1d3085c033635a880f478d49780c72faaba50
Robert Norris <rob@eatenbyagrue.org>
2011-02-12 15:17:56 +1100

    equipment constants

--- src/LuaConstants.cpp
+++ src/LuaConstants.cpp
@@ -1,6 +1,7 @@
 #include "LuaConstants.h"
 #include "StarSystem.h"
 #include "Polit.h"
+#include "EquipType.h"
 
 #define _setfield(v,k) do { lua_pushinteger(L, v); lua_setfield(L, -2, k); } while(0)
 
@@ -107,4 +108,111 @@ void LuaConstants::RegisterConstants(lua_State *L)
 	lua_setfield(L, -2, "GovType");
 
 	lua_setfield(L, LUA_GLOBALSINDEX, "Polit");
+
+
+	lua_newtable(L); // EquipType
+
+	lua_newtable(L); // Slot
+	_setfield(Equip::SLOT_CARGO,            "SLOT_CARGO");
+	_setfield(Equip::SLOT_ENGINE,           "SLOT_ENGINE");
+	_setfield(Equip::SLOT_LASER,            "SLOT_LASER");
+	_setfield(Equip::SLOT_MISSILE,          "SLOT_MISSILE");
+	_setfield(Equip::SLOT_ECM,              "SLOT_ECM");
+	_setfield(Equip::SLOT_SCANNER,          "SLOT_SCANNER");
+	_setfield(Equip::SLOT_RADARMAPPER,      "SLOT_RADARMAPPER");
+	_setfield(Equip::SLOT_HYPERCLOUD,       "SLOT_HYPERCLOUD");
+	_setfield(Equip::SLOT_HULLAUTOREPAIR,   "SLOT_HULLAUTOREPAIR");
+	_setfield(Equip::SLOT_ENERGYBOOSTER,    "SLOT_ENERGYBOOSTER");
+	_setfield(Equip::SLOT_ATMOSHIELD,       "SLOT_ATMOSHIELD");
+	_setfield(Equip::SLOT_FUELSCOOP,        "SLOT_FUELSCOOP");
+	_setfield(Equip::SLOT_LASERCOOLER,      "SLOT_LASERCOOLER");
+	_setfield(Equip::SLOT_CARGOLIFESUPPORT, "SLOT_CARGOLIFESUPPORT");
+	_setfield(Equip::SLOT_AUTOPILOT,        "SLOT_AUTOPILOT");
+	lua_setfield(L, -2, "Slot");
+
+	lua_newtable(L); // Type
+	_setfield(Equip::NONE,                  "NONE");
+	_setfield(Equip::HYDROGEN,              "HYDROGEN");
+	_setfield(Equip::LIQUID_OXYGEN,         "LIQUID_OXYGEN");
+	_setfield(Equip::METAL_ORE,             "METAL_ORE");
+	_setfield(Equip::CARBON_ORE,            "CARBON_ORE");
+	_setfield(Equip::METAL_ALLOYS,          "METAL_ALLOYS");
+	_setfield(Equip::PLASTICS,              "PLASTICS");
+	_setfield(Equip::FRUIT_AND_VEG,         "FRUIT_AND_VEG");
+	_setfield(Equip::ANIMAL_MEAT,           "ANIMAL_MEAT");
+	_setfield(Equip::LIVE_ANIMALS,          "LIVE_ANIMALS");
+	_setfield(Equip::LIQUOR,                "LIQUOR");
+	_setfield(Equip::GRAIN,                 "GRAIN");
+	_setfield(Equip::TEXTILES,              "TEXTILES");
+	_setfield(Equip::FERTILIZER,            "FERTILIZER");
+	_setfield(Equip::WATER,                 "WATER");
+	_setfield(Equip::MEDICINES,             "MEDICINES");
+	_setfield(Equip::CONSUMER_GOODS,        "CONSUMER_GOODS");
+	_setfield(Equip::COMPUTERS,             "COMPUTERS");
+	_setfield(Equip::ROBOTS,                "ROBOTS");
+	_setfield(Equip::PRECIOUS_METALS,       "PRECIOUS_METALS");
+	_setfield(Equip::INDUSTRIAL_MACHINERY,  "INDUSTRIAL_MACHINERY");
+	_setfield(Equip::FARM_MACHINERY,        "FARM_MACHINERY");
+	_setfield(Equip::MINING_MACHINERY,      "MINING_MACHINERY");
+	_setfield(Equip::AIR_PROCESSORS,        "AIR_PROCESSORS");
+	_setfield(Equip::SLAVES,                "SLAVES");
+	_setfield(Equip::HAND_WEAPONS,          "HAND_WEAPONS");
+	_setfield(Equip::BATTLE_WEAPONS,        "BATTLE_WEAPONS");
+	_setfield(Equip::NERVE_GAS,             "NERVE_GAS");
+	_setfield(Equip::NARCOTICS,             "NARCOTICS");
+	_setfield(Equip::MILITARY_FUEL,         "MILITARY_FUEL");
+	_setfield(Equip::RUBBISH,               "RUBBISH");
+	_setfield(Equip::RADIOACTIVES,          "RADIOACTIVES");
+
+	_setfield(Equip::MISSILE_UNGUIDED,      "MISSILE_UNGUIDED");
+	_setfield(Equip::MISSILE_GUIDED,        "MISSILE_GUIDED");
+	_setfield(Equip::MISSILE_SMART,         "MISSILE_SMART");
+	_setfield(Equip::MISSILE_NAVAL,         "MISSILE_NAVAL");
+	_setfield(Equip::ATMOSPHERIC_SHIELDING, "ATMOSPHERIC_SHIELDING");
+	_setfield(Equip::ECM_BASIC,             "ECM_BASIC");
+	_setfield(Equip::SCANNER,               "SCANNER");
+	_setfield(Equip::ECM_ADVANCED,          "ECM_ADVANCED");
+	_setfield(Equip::SHIELD_GENERATOR,      "SHIELD_GENERATOR");
+	_setfield(Equip::LASER_COOLING_BOOSTER, "LASER_COOLING_BOOSTER");
+	_setfield(Equip::CARGO_LIFE_SUPPORT,    "CARGO_LIFE_SUPPORT");
+	_setfield(Equip::AUTOPILOT,             "AUTOPILOT");
+	_setfield(Equip::RADAR_MAPPER,          "RADAR_MAPPER");
+	_setfield(Equip::FUEL_SCOOP,            "FUEL_SCOOP");
+	_setfield(Equip::HYPERCLOUD_ANALYZER,   "HYPERCLOUD_ANALYZER");
+	_setfield(Equip::HULL_AUTOREPAIR,       "HULL_AUTOREPAIR");
+	_setfield(Equip::SHIELD_ENERGY_BOOSTER, "SHIELD_ENERGY_BOOSTER");
+
+	_setfield(Equip::DRIVE_CLASS1,          "DRIVE_CLASS1");
+	_setfield(Equip::DRIVE_CLASS2,          "DRIVE_CLASS2");
+	_setfield(Equip::DRIVE_CLASS3,          "DRIVE_CLASS3");
+	_setfield(Equip::DRIVE_CLASS4,          "DRIVE_CLASS4");
+	_setfield(Equip::DRIVE_CLASS5,          "DRIVE_CLASS5");
+	_setfield(Equip::DRIVE_CLASS6,          "DRIVE_CLASS6");
+	_setfield(Equip::DRIVE_CLASS7,          "DRIVE_CLASS7");
+	_setfield(Equip::DRIVE_CLASS8,          "DRIVE_CLASS8");
+	_setfield(Equip::DRIVE_CLASS9,          "DRIVE_CLASS9");
+	_setfield(Equip::DRIVE_MIL1,            "DRIVE_MIL1");
+	_setfield(Equip::DRIVE_MIL2,            "DRIVE_MIL2");
+	_setfield(Equip::DRIVE_MIL3,            "DRIVE_MIL3");
+	_setfield(Equip::DRIVE_MIL4,            "DRIVE_MIL4");
+
+	_setfield(Equip::PULSECANNON_1MW,       "PULSECANNON_1MW");
+	_setfield(Equip::PULSECANNON_DUAL_1MW,  "PULSECANNON_DUAL_1MW");
+	_setfield(Equip::PULSECANNON_2MW,       "PULSECANNON_2MW");
+	_setfield(Equip::PULSECANNON_RAPID_2MW, "PULSECANNON_RAPID_2MW");
+	_setfield(Equip::PULSECANNON_4MW,       "PULSECANNON_4MW");
+	_setfield(Equip::PULSECANNON_10MW,      "PULSECANNON_10MW");
+	_setfield(Equip::PULSECANNON_20MW,      "PULSECANNON_20MW");
+	_setfield(Equip::MININGCANNON_17MW,     "MININGCANNON_17MW");
+	_setfield(Equip::SMALL_PLASMA_ACCEL,    "SMALL_PLASMA_ACCEL");
+	_setfield(Equip::LARGE_PLASMA_ACCEL,    "LARGE_PLASMA_ACCEL");
+
+	_setfield(Equip::TYPE_MAX,              "TYPE_MAX");
+	_setfield(Equip::FIRST_COMMODITY,       "FIRST_COMMODITY");
+	_setfield(Equip::LAST_COMMODITY,        "LAST_COMMODITY");
+	_setfield(Equip::FIRST_SHIPEQUIP,       "FIRST_SHIPEQUIP");
+	_setfield(Equip::LAST_SHIPEQUIP,        "LAST_SHIPEQUIP");
+	lua_setfield(L, -2, "Type");
+
+	lua_setfield(L, LUA_GLOBALSINDEX, "EquipType");
 }
