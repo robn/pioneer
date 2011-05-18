3ee235d4d50a194b596221d13be69e8e4bc37816
Robert Norris <rob@eatenbyagrue.org>
2011-05-18 15:53:21 +1000

    switch to TitilliumText22L Bold 14px and light (no x hinting apparently)

--- src/glfreetype.cpp
+++ src/glfreetype.cpp
@@ -577,11 +577,11 @@ TextureFontFace::TextureFontFace(const char *filename_ttf, int a_width, int a_he
 			memset(pixBuf, 0, 2*sz*sz);
 
 			unsigned int glyph_index = FT_Get_Char_Index(m_face, chr);
-			if (FT_Load_Glyph(m_face, glyph_index, FT_LOAD_FORCE_AUTOHINT) != 0) {
+			if (FT_Load_Glyph(m_face, glyph_index, FT_LOAD_TARGET_LIGHT) != 0) {
 				fprintf(stderr, "couldn't load glyph for '%c'\n", chr);
 				continue;
 			}
-			FT_Render_Glyph(m_face->glyph, FT_RENDER_MODE_NORMAL);
+			FT_Render_Glyph(m_face->glyph, FT_RENDER_MODE_LIGHT);
 
 			// face->glyph->bitmap
 			// copy to square buffer GL can stomach
