2dfebdc1d8fd02fe56c0a01ffcbc275911fc3d4c
Robert Norris <rob@eatenbyagrue.org>
2011-08-25 08:26:17 +1000

    fixed lib lowercasing and constructor call

--- data/libs/fixed.lua
+++ data/libs/fixed.lua
@@ -1,11 +1,11 @@
 -- additional fixed-point utilities
 
 -- f - shorthand method to create a new fixed-point number
-function f (...) return Fixed:New(...) end
+function f (...) return fixed.new(...) end
 
 
 -- math library equivalent for fixed-point numbers
 math.fixed = {}
 
 -- math.fixed.deg2rad - convert degrees to radians (fixed)
-math.fixed.deg2rad = function (n) return n * Fixed:New(31416,1800000) end
+math.fixed.deg2rad = function (n) return n * fixed.new(31416,1800000) end
