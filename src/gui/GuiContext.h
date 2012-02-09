60cc8f121f60d3fda99453016eebe7882055a229
Robert Norris <rob@eatenbyagrue.org>
2012-02-10 08:05:13 +1100

    strip the context right back to the bones. eventually it will have all sorts of things but I'm not ready for it yet

--- src/gui/GuiContext.h
+++ src/gui/GuiContext.h
@@ -1,54 +1,17 @@
 #ifndef _GUICONTEXT_H
-#define _GUICONTEXT_H 1
-
-#include "libs.h"
-#include <stack>
-
-class FontManager;
-class TextureCache;
-class TextureFont;
+#define _GUICONTEXT_H
 
 namespace Gui {
 
-class Screen;
+// The GUI context holds resources that are shared by all widgets. Examples of
+// such resources are fonts, default styles, textures and so on. Each widget
+// recieves a pointer to its context when it is added to a container, and can
+// access it using its GetContext() method. Widgets that are not in a
+// container do not have a context.
 
 class Context {
 public:
-	Context(int screen_width, int screen_height, int ui_width, int ui_height);
-	~Context();
-
-	void HandleSDLEvent(SDL_Event *event);
-	void Draw();
-	void MainLoopIteration();
-	sigc::connection AddTimer(Uint32 ms, sigc::slot<void> slot);
-
-
-	FontManager *GetFontManager() { return m_fontManager.Get(); }
-
-	void PushFont(TextureFont* f) { m_fontStack.push(f); }
-	void PushFont(const std::string &name);
-	void PopFont() { m_fontStack.pop(); };
-	TextureFont *GetFont() { return m_fontStack.size() ? m_fontStack.top() : m_defaultFont; }
-	TextureFont *GetDefaultFont() { return m_defaultFont; }
-
-	TextureCache *GetTextureCache() { return m_textureCache.Get(); }
-
-
-	Screen *screen;
-
-private:
-	struct TimerSignal {
-		TimerSignal(Uint32 _goTime) : goTime(_goTime) {}
-		Uint32 goTime;
-		sigc::signal<void> sig;
-	};
-	std::list<TimerSignal*> m_timeSignals;
-
-	ScopedPtr<FontManager> m_fontManager;
-	std::stack<TextureFont*> m_fontStack;
-	TextureFont *m_defaultFont;
-
-	ScopedPtr<TextureCache> m_textureCache;
+	Context();
 };
 
 }
