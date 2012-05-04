079cb6f65cfc9df4c7d3dd3c71934db258b361d0
Robert Norris <rob@eatenbyagrue.org>
2012-05-04 23:19:10 +1000

    stub Tagged mixin class

---
+++ src/Tagged.cpp
@@ -0,0 +1,7 @@
+#include "Tagged.h"
+#include "LuaUtils.h"
+
+unsigned int Tagged::AddTagsFromLua(lua_State *L, int stackIndex)
+{
+	return 0;
+}
---
+++ src/Tagged.h
@@ -0,0 +1,27 @@
+#ifndef _TAGGED_H
+#define _TAGGED_H
+
+#include <set>
+#include <string>
+
+struct lua_State;
+
+class Tagged {
+public:
+	void AddTag(const std::string &tag) { m_set.insert(tag); }
+	void RemoveTag(const std::string &tag) { m_set.erase(tag); }
+	bool HasTag(const std::string &tag) const { return m_set.find(tag) != m_set.end(); }
+
+	typedef std::set<std::string>::const_iterator TagIterator;
+	const TagIterator TagsBegin() const { return m_set.begin(); }
+	const TagIterator TagsEnd() const { return m_set.end(); }
+
+	// adds tags from lua table. takes lua state and stack index of table.
+	// returns number of tags added
+	unsigned int AddTagsFromLua(lua_State *L, int stackIndex);
+
+private:
+	std::set<std::string> m_set;
+};
+
+#endif
