10ee8137eb181fddab7c442eb1fdde779dff34bc
Robert Norris <rob@eatenbyagrue.org>
2012-05-08 08:26:44 +1000

    add tags. game now starts :o

--- src/Tagged.cpp
+++ src/Tagged.cpp
@@ -1,7 +1,22 @@
 #include "Tagged.h"
 #include "LuaUtils.h"
 
-unsigned int Tagged::AddTagsFromLua(lua_State *L, int stackIndex)
+unsigned int Tagged::AddTagsFromLua(lua_State *l, int stackIndex)
 {
-	return 0;
+	unsigned int count = 0;
+
+	LUA_DEBUG_START(l);
+
+	lua_pushvalue(l, stackIndex);
+	lua_pushnil(l);
+	while (lua_next(l, -2)) {
+		AddTag(luaL_checkstring(l, -1));
+		lua_pop(l, 1);
+		count++;
+	}
+	lua_pop(l, 1);
+
+	LUA_DEBUG_END(l, 0);
+	
+	return count;
 }
