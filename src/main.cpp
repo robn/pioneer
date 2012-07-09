9bd32093ec2ffceb390777591a0286974a452e4a
Robert Norris <rob@eatenbyagrue.org>
2012-07-09 22:34:16 +1000

    hackjob run switch to dump system and exit

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include "libs.h"
 #include "Pi.h"
 #include <signal.h>
+#include <getopt.h>
+#include "galaxy/SystemPath.h"
+#include "galaxy/StarSystem.h"
 
 void sigsegv_handler(int signum)
 {
@@ -11,10 +14,31 @@ void sigsegv_handler(int signum)
 	}
 }
 
-int main(int argc, char**)
+int main(int argc, char **argv)
 {
+	const char *path = 0;
+
+	int opt;
+	while ((opt = getopt(argc, argv, "s:h?")) >= 0) {
+		switch (opt) {
+			case 's':
+				path = optarg;
+				break;
+
+			default:
+				printf("usage: %s [-s path]\n", argv[0]);
+				return -1;
+		}
+	}
+
 //	signal(SIGSEGV, sigsegv_handler);
 	Pi::Init();
+
+	if (path) {
+		StarSystem::GetCached(SystemPath::Parse(path))->Dump();
+		return 0;
+	}
+
 	for (;;) Pi::Start();
 	return 0;
 }
