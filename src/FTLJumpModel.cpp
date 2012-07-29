9edc28e4b746413f23ab9021718fc6ab084d55f9
Robert Norris <rob@eatenbyagrue.org>
2012-07-29 20:44:19 +0200

    initial stub for ftl jump class

---
+++ src/FTLJumpModel.cpp
@@ -0,0 +1,7 @@
+#include "FTLJumpModel.h"
+
+FTLJumpModel::FTLJumpModel(Ship *ship, Body *target) :
+	m_ship(ship),
+	m_target(target)
+{
+}
---
+++ src/FTLJumpModel.h
@@ -0,0 +1,16 @@
+#ifndef _FTLJUMPMODEL_H
+#define _FTLJUMPMODEL_H
+
+class Ship;
+class Body;
+
+class FTLJumpModel {
+public:
+	FTLJumpModel(Ship *ship, Body *target);
+
+private:
+	Ship *m_ship;
+	Body *m_target;
+};
+
+#endif
