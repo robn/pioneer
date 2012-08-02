c7fe1a225822ae8415b799a17740affe0493d07b
Simon Chopin <chopin.simon@gmail.com>
2012-08-02 13:48:10 +0200

    new-equipment: Add an "equipSet" Property to Ship to hold the new lua EquipSet
    Conflicts:
    	src/Ship.h

--- src/Ship.h
+++ src/Ship.h
@@ -17,6 +17,7 @@
 #include "ShipType.h"
 #include "scenegraph/SceneGraph.h"
 #include "scenegraph/ModelSkin.h"
+#include "LuaTable.h"
 #include <list>
 #include <unordered_map>
 
@@ -130,6 +131,8 @@ public:
 	float GetWheelState() const { return m_wheelState; }
 	int GetWheelTransition() const { return m_wheelTransition; }
 	bool SpawnCargo(CargoBody * c_body) const;
+
+	LuaRef GetEquipSet() const { return m_equipSet; }
 
 	virtual bool IsInSpace() const { return (m_flightState != HYPERSPACE); }
 
@@ -301,6 +304,8 @@ protected:
 
 	ShipController *m_controller;
 
+	LuaRef m_equipSet;
+
 private:
 	float GetECMRechargeTime();
 	void DoThrusterSounds() const;
@@ -310,7 +315,7 @@ private:
 	void TestLanded();
 	void UpdateAlertState();
 	void UpdateFuel(float timeStep, const vector3d &thrust);
-    void SetShipId(const ShipType::Id &shipId);
+	void SetShipId(const ShipType::Id &shipId);
 	void OnEquipmentChange(Equip::Type e);
 	void EnterHyperspace();
 	void InitGun(const char *tag, int num);
