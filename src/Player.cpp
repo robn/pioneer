4fcd9f4712793948b1b0d5252180646179ecb17d
Robert Norris <rob@eatenbyagrue.org>
2013-06-24 17:14:40 +1000

    remove unused method

--- src/Player.cpp
+++ src/Player.cpp
@@ -48,12 +48,6 @@ bool Player::OnDamage(Object *attacker, float kgDamage)
 	return r;
 }
 
-//XXX handle killcounts in lua
-void Player::SetDockedWith(SpaceStation *s, int port)
-{
-	Ship::SetDockedWith(s, port);
-}
-
 //XXX all ships should make this sound
 bool Player::SetWheelState(bool down)
 {
